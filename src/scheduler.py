b2903288cc93ab5f21ffb949515b0a8d6a900d5a
Hongyi Wu <hongyi@HongyideMac-mini-2119.local>
2023-07-09 00:03:20 -0400

    debug scheduler2

--- src/scheduler.py
+++ src/scheduler.py
@@ -5,14 +5,15 @@ from src.utils.dailyReport import get_daily_report
 from src.utils.toNotion import save_to_notion
 # 定义定时任务
 def clear_history():
-    report = get_daily_report()['choices'][0]['message']['content']
-    save = save_to_notion(datetime.now().date(), report)
-    notes.clear()
+    # report = get_daily_report()['choices'][0]['message']['content']
+    # save = save_to_notion(datetime.now().date(), report)
+    # notes.clear()
+    print('boom!')
     
 
 # 创建后台调度器
 scheduler = BackgroundScheduler()
 
 # 添加定时任务，每天11:30执行
-scheduler.add_job(clear_history, 'cron', hour=23, minute=51)
+scheduler.add_job(clear_history, 'cron', hour=0, minute=5)
 
